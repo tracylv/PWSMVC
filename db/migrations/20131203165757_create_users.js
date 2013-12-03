c83c295102cea2923f728f6899ca65bb34db90c5
Tracy Lv <lv_tengfei@163.com>
2013-12-03 18:14:08 +0800

    Initial commit

---
+++ db/migrations/20131203165757_create_users.js
@@ -0,0 +1,31 @@
+var CreateUsers = function () {
+  this.up = function (next) {
+    var def = function (t) {
+          t.column('name', 'string');
+          t.column('age', 'int');
+        }
+      , callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.createTable('user', def, callback);
+  };
+
+  this.down = function (next) {
+    var callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.dropTable('user', callback);
+  };
+};
+
+exports.CreateUsers = CreateUsers;
