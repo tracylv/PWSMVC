499ea0f6663d6b7a562cf1f3af4c48ccaefc2e61
Tracy Lv <lv_tengfei@163.com>
2013-12-05 18:13:50 +0800

    update

---
+++ db/migrations/20131204102424_create_comments.js
@@ -0,0 +1,31 @@
+var CreateComments = function () {
+  this.up = function (next) {
+    var def = function (t) {
+          t.column('title', 'string');
+          t.column('status', 'string');
+        }
+      , callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.createTable('comment', def, callback);
+  };
+
+  this.down = function (next) {
+    var callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.dropTable('comment', callback);
+  };
+};
+
+exports.CreateComments = CreateComments;
